f8ea2601c5f90cf08f7d245514a72c0064ee5b4b
Ahmed Nehran Nifazdeen <nehrannifazdeen128@gmail.com>
2025-03-17 21:05:54 +0530

    What I practiced on the 17th of March 2025

---
+++ 17th of March.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+/*        Scanner scanner = new Scanner(System.in);
+        String num=scanner.nextLine();
+        // Write your code below
+        if(num.equals("1")){
+            System.out.println("T");
+        }else{
+            System.out.println("F");
+        }
+        scanner.close();
+
+        System.out.println("Calculator App");
+        Scanner scan1= new Scanner(System.in);
+
+
+        System.out.print("Enter first number: ");
+        Double num1=scan1.nextDouble();
+
+        System.out.print("Enter second number: ");
+        Double num2=scan1.nextDouble();
+
+        System.out.println("First number: " + num1);
+        System.out.print("Second number: " + num2);*/
+
+        String hi="Hello, world";
+        int num=25;
+        double fl=123.2567;
+
+
+        System.out.printf("String: %s, Integer: %d, Floating-point: %.2f%n", hi, num, fl);
+
+        int age = 20;
+        String name = "John";
+        System.out.printf("Name: %s, Age: %d",name,age);
+        System.out.println();
+
+        String pro="product";
+        int quantity=10;
+        double price= 19.99;
+
+        System.out.printf("Price : %s, quantity: %d, Price: %.2f%n",pro,quantity, price);
+        System.out.println();
+
+        String result= "The result is: ";
+        int num1=42;
+        double point=3.14;
+        System.out.printf(result+num1+" "+point);
+
+        
+
+
+    }}
