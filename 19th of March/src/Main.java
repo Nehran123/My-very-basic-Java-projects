caf9e1ae23e13238c04ca471b86a6fbef0c0d43c
Ahmed Nehran Nifazdeen <nehrannifazdeen128@gmail.com>
2025-03-19 21:37:38 +0530

    Add files via upload

---
+++ 19th of March/src/Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*
+        for(int i=3;i<=27;i++){
+            System.out.println("Hello Coddy: "+ i);
+        }
+        Scanner scanner = new Scanner(System.in);
+        double num= scanner.nextDouble();
+        while(num >= 3.5)
+        {
+            num=num/2;
+
+
+
+        }
+        System.out.println(num);
+*/
+
+        int sum = 0;
+        int number = 1;
+
+        // Your code here
+        do {
+            sum = sum + number;
+            number += 2;
+
+
+            System.out.println("Sum is: " + sum);
+            System.out.println("Num is: " + number);
+        } while (number <= 50);
+
+
+        // Print the final sum
+        System.out.println("Final Sum: " + sum);
+/*
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+
+            if (i == 15) {
+                break;
+            }*/
+            for (int in = 1; in <= 20; in++) {
+                System.out.println(in);
+                if (10 / 2 0){
+                    continue;
+                }
+            }
+        }
+        }
+
+
+
+
+
+
+
+
